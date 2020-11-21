70a09b7bd8e1f33329bece8379c0e2394e199c32
Mitradis <mitradis@gmail.com>
2020-11-21 16:48:38 +0300

    Clear Upload

---
+++ InFilesFinder/FormMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace InFilesFinder
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(181, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Путь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaxLength = 0;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(495, 251);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.WordWrap = false;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(405, 275);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 24);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Ключ(и)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 316);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "In Files Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
---
+++ InFilesFinder/FormMain.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InFilesFinder
+{
+    public partial class FormMain : Form
+    {
+        List<string> filesList = new List<string>();
+        List<string> searchStrings = new List<string>();
+        string temppath = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            toolTip1.SetToolTip(checkBox1, "Производит поиск каждой строки в дополнительных символах в начале и в конце.");
+        }
+
+        private void searthFolder(string path)
+        {
+            foreach (string line in Directory.GetDirectories(path))
+            {
+                searthFolder(line);
+                getInFilesFinder(line);
+            }
+        }
+
+        private void getInFilesFinder(string path)
+        {
+            foreach (string line in Directory.GetFiles(path))
+            {
+                using (StreamReader sr = new StreamReader(line))
+                {
+                    sr.BaseStream.Position = 0;
+                    string line1;
+                    int linenum = 0;
+                    while ((line1 = sr.ReadLine()) != null)
+                    {
+                        linenum++;
+                        bool find = false;
+                        foreach (string line2 in searchStrings)
+                        {
+                            if (!checkBox1.Checked)
+                            {
+                                if (line1.IndexOf(line2, StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                            }
+                            else
+                            {
+                                if (line1.IndexOf("=" + line2, StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                                else if (line1.IndexOf("(" + line2 + ")", StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                                else if (line1.IndexOf("[" + line2 + "]", StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                                else if (line1.IndexOf("\"" + line2 + "\"", StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                                else if (line1.IndexOf("'" + line2 + "'", StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                                else if (line1.IndexOf("," + line2 + ",", StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                                else if (line1.IndexOf(" " + line2 + ",", StringComparison.OrdinalIgnoreCase) >= 0)
+                                {
+                                    find = true;
+                                }
+                            }
+                            if (find)
+                            {
+                                filesList.Add(line + "\t" + line2 + "\t" + linenum.ToString());
+                                find = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (temppath != null)
+            {
+                folderBrowserDialog1.SelectedPath = temppath;
+            }
+            else
+            {
+                folderBrowserDialog1.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            searchStrings.AddRange(textBox1.Lines);
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK && searchStrings.Count > 0)
+            {
+                if (Directory.Exists(folderBrowserDialog1.SelectedPath))
+                {
+                    button1.Enabled = false;
+                    checkBox1.Enabled = false;
+                    temppath = folderBrowserDialog1.SelectedPath;
+                    getInFilesFinder(folderBrowserDialog1.SelectedPath);
+                    searthFolder(folderBrowserDialog1.SelectedPath);
+                    File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\_foundlist.txt", filesList);
+                    checkBox1.Enabled = true;
+                    button1.Enabled = true;
+                }
+            }
+            searchStrings.Clear();
+            filesList.Clear();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                if (sender != null)
+                    ((TextBox)sender).SelectAll();
+            }
+        }
+    }
+}
